275efd3b055261b8651df74fb114de49883c6aca
Rakshit Rabugotra <rakshit.rabugotra000@gmail.com>
2024-08-23 19:12:03 +0530

    feat: add movie poster image support

--- api/app/lib/movie_recommender.py
+++ api/app/lib/movie_recommender.py
@@ -1,15 +1,25 @@
 # Use MultiLabelBinarizer to one-hot encode the genres
 from sklearn.preprocessing import MultiLabelBinarizer
 from sklearn.metrics.pairwise import cosine_similarity
+import requests
 
 # For instant search results from the database
 from rapidfuzz import process as rapid_process, fuzz as rapid_fuzz
+from logger import logger
 import pandas as pd
+import os
+import re
+
+api_key = os.environ.get("MOVIE_DB_API_KEY") or ""
 
 
 class MovieRecommender:
     # The default path to the recommendations file
     path_to_movies = r"./ml-20m/movies.csv"
+    # The poster api base URL
+    poster_image_base_url = "http://image.tmdb.org/t/p/w500/"
+    poster_api_base = "https://api.themoviedb.org/3/search/movie?api_key=" + api_key
+    poster_search = "&query={0}&year={1}"
 
     def __init__(self, movies: pd.DataFrame):
         """
@@ -75,6 +85,12 @@ class MovieRecommender:
         recommended_titles = self.movies[
             self.movies["movieId"].isin(recommended_movie_ids)
         ]
+
+        # Add the image with the movie
+        recommended_titles["image"] = recommended_titles["title"].apply(
+            lambda title: self.__fetch_movie_poster(title)
+        )
+
         # Sort the filtered DataFrame by rating in descending order
         return recommended_titles.sort_values(by="rating", ascending=False)
 
@@ -108,6 +124,45 @@ class MovieRecommender:
 
         # Limit the number of results to return
         return results.head(limit)
+
+    def __fetch_movie_poster(self, movieTitle: str):
+        """
+        Fetches the Movie poster of the movie searched using 'themoviedb' API
+        """
+        title, year = self.__reformat_title(movieTitle)
+
+        endpoint = (
+            MovieRecommender.poster_api_base
+            + MovieRecommender.poster_search.format(title, year)
+        )
+        # Get the response from the endpoint
+        try:
+            response = requests.get(endpoint)
+            poster_path = (
+                MovieRecommender.poster_image_base_url
+                + response.json()["results"][0]["poster_path"]
+            )
+            return poster_path
+        except Exception as e:
+            logger.error(
+                f"Error while fetching movie poster for title: '{movieTitle}' | error: {e}"
+            )
+            return None
+
+    def __reformat_title(self, original_title: str):
+        # Check if the title is in the format "Thing, The (1982)"
+        match = re.match(r"^(.*),\s*The\s+\((\d{4})\)$", original_title)
+        if match:
+            title, year = match.groups()
+            return title, int(year)
+
+        # Check if the title is in the format "Gone Girl (2014)" or "Frailty (2001)"
+        match = re.match(r"^(.*)\s+\((\d{4})\)$", original_title)
+        if match:
+            title, year = match.groups()
+            return title, int(year)
+
+        return "", 2020
 
     def __initialize_model(self):
         """
